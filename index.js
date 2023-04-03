44a5eb40c42f0ec300abc880280681fd0e7e1d98
janafojtova <124291649+janafojtova@users.noreply.github.com>
2023-04-03 15:32:32 +0200

    Vypracování úkolu

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log('Funguju')
+const kameny = document.querySelectorAll(".kamen");
+
+const hrajTon = (event) =>{
+    let nadpis = document.querySelector("h1");
+    let jmenoKamene = event.textContent;
+    nadpis.textContent = jmenoKamene;
+    const zvuk = new Audio("tony/"+jmenoKamene+".mp3")
+    zvuk.play() 
+
+}
+const stiskKlavesy =(udalost)=>{
+    hrajTon(udalost.target);
+}
+
+for (let index = 0; index < kameny.length; index++) {
+    kameny[index].addEventListener("click",stiskKlavesy);    
+}
